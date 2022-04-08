6f35ab2bc8a0aa87cbab4cfdde56c0744ec8817b
tasin <tasin98765@gmail.com>
2022-04-09 00:41:52 +0600

    All file added

---
+++ orders.js
@@ -0,0 +1,32 @@
+const Orders = [
+    {
+        productName: "Foldable Mini Drone",
+        productNumber: 39752, 
+        paymentStatus: "due",
+        shipping: "Pending" 
+    },
+    {
+        productName: "DJI FPV",
+        productNumber: 68450, 
+        paymentStatus: "Refunded",
+        shipping: "Declined" 
+    },
+    {
+        productName: "Phantom 4 Pro Drone",
+        productNumber: 89863, 
+        paymentStatus: "Paid",
+        shipping: "Delivered" 
+    },
+    {
+        productName: "4K GPS Drone",
+        productNumber: 79845, 
+        paymentStatus: "Refunded",
+        shipping: "Delivered" 
+    },
+    {
+        productName: "Skydio TOY DRONE",
+        productNumber: 75208, 
+        paymentStatus: "due",
+        shipping: "Declined" 
+    }
+]
